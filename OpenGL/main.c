1f0889eb3748a90872c49009ce9096221a7fe780
Cewein <pro.mnowak@gmail.com>
2019-01-09 23:52:07 +0100

    add light struct

--- OpenGL/main.c
+++ OpenGL/main.c
@@ -10,12 +10,13 @@
 #include "OpenGLFunction.h"
 #include "ShaderFunction.h"
 #include "glTexture.h"
+#include "lightGL.h"
 
 #define SCR_WIDTH 800
 #define SCR_HEIGHT 600
 
 int main()
-{ 
+{
 	//FILE * logFile = NULL;
 
 	glfwInit();
@@ -88,8 +89,8 @@ int main()
 	 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
 	-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
 	-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
-	}; 
-	
+	};
+
 	vec3 cubePos[] = {
 	{ 0.0f,  0.0f,  0.0f  },
 	{ 2.0f,  5.0f, -15.0f },
@@ -103,6 +104,8 @@ int main()
 	{ -1.3f,  1.0f, -1.5f }
 	};
 
+	/////////////////////// LOADING OBJ /////////////////////// 
+
 	tinyobj_attrib_t attrib;
 	tinyobj_shape_t * shapes = NULL;
 	int num_shapes;
@@ -112,7 +115,7 @@ int main()
 	int data_len = 0;
 
 	FILE * objFile = NULL;
-	objFile = fopen("JUPITER.obj", "rb");
+	objFile = fopen("cube.obj", "rb");
 
 	data_len = fsize(objFile);
 	char * data = freadInArray(objFile);
@@ -120,49 +123,77 @@ int main()
 	unsigned int flags = TINYOBJ_FLAG_TRIANGULATE;
 	int ret = tinyobj_parse_obj(&attrib, &shapes, &num_shapes, &materials, &num_materials, data, data_len, flags);
 
-	printf("# of shapes    = %d\n", (int)num_shapes);
-	printf("# of materials = %d\n", (int)num_materials);
-	printf("# name diffure = %s\n", materials->diffuse_texname);
+	printf("# of shapes     = %d\n", (int)num_shapes);
+	printf("# of materials  = %d\n", (int)num_materials);
+	printf("# of vertices   = %d\n", (int)attrib.num_vertices);
+	printf("# of face       = %d\n", (int)attrib.num_faces);
+	//printf("name diffure    = %s\n", materials->diffuse_texname);
+	printf("sizeof vertices = %d\n", sizeof(float) * attrib.num_vertices);
+	printf("sizeof cube arr = %d\n", sizeof(cube));
 
-	tinyobj_attrib_free(&attrib);
-	tinyobj_shapes_free(shapes, num_shapes);
-	tinyobj_materials_free(materials, num_materials);
+	for (int i = 0; i < attrib.num_face_num_verts; i++) printf("indice %d : %f\n", i, attrib.material_ids[i]);
+
+
+	///////////////////////////////////////////////////////////
+
+	unsigned int jupElm;
+	unsigned int jupVBO;
+	unsigned int jupVAO;
+
+	{
+		glGenVertexArrays(1, &jupVAO);
+		glGenBuffers(1, &jupVBO);
+		glGenBuffers(1, &jupElm);
+
+		glBindVertexArray(jupVAO);
+
+		glBindBuffer(GL_ARRAY_BUFFER, jupVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * attrib.num_vertices, attrib.vertices, GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, jupElm);
+		//glBufferData(GL_ELEMENT_ARRAY_BUFFER, attrib.);
+
+	}
+
 
 	//---------------------- BUFFER ----------------------//
 	//creating buffer
 	unsigned int VBO;
 	unsigned int cubeVAO;
-	unsigned int EBO;
 
-	//ALWAYS GEN BUFFER OR ARRAY BEFORE USING THEM//
-	glGenVertexArrays(1, &cubeVAO);
-	glGenBuffers(1, &VBO);
+	{
+		//ALWAYS GEN BUFFER OR ARRAY BEFORE USING THEM//
+		glGenVertexArrays(1, &cubeVAO);
+		glGenBuffers(1, &VBO);
 
-	//bind the Vertex Array Object (VAO), next the Vertex Buffer (VBO) and last the Element Buffer/Vertex Attribute (EBO)
-	glBindVertexArray(cubeVAO);
+		//bind the Vertex Array Object (VAO), next the Vertex Buffer (VBO) and last the Element Buffer/Vertex Attribute (EBO)
+		glBindVertexArray(cubeVAO);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(cube), cube, GL_STATIC_DRAW);
-	
-	//box vertex attribute
-	//telling OpenGL how to use the vertices array
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(cube), cube, GL_STATIC_DRAW);
+
+		//box vertex attribute
+		//telling OpenGL how to use the vertices array
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+		glEnableVertexAttribArray(2);
+	}
 
 	//light buffer
 	unsigned int lightVAO;
 
-	glGenVertexArrays(1,&lightVAO);
+	{
+		glGenVertexArrays(1, &lightVAO);
 
-	glBindVertexArray(lightVAO);
-	glBindBuffer(GL_ARRAY_BUFFER,VBO);
+		glBindVertexArray(lightVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+	}
 
 	//enable the z buffer
 	glEnable(GL_DEPTH_TEST);
@@ -171,17 +202,21 @@ int main()
 	//here linking all shader together
 	unsigned int shaderProgram;
 	unsigned int lightProgram;
+	unsigned int jupProg;
 	
 	addProgShader("shader\\vertex.glsl", "shader\\texture.glsl", &shaderProgram);
 	addProgShader("shader\\vertexNotexture.glsl", "shader\\lightFrag.glsl", &lightProgram);
+	addProgShader("shader\\vertexNotexture.glsl", "shader\\lightFrag.glsl", &jupProg);
+
 
 	//------------------ TEXTURE OPTION ------------------//
 
 	unsigned int boxTexture;
-	createTexture(&boxTexture, "texture\\container2.png", FALSE);
 	unsigned int boxSpecular;
-	createTexture(&boxSpecular, "texture\\container2_specular.png", FALSE);
 	unsigned int boxEmissive;
+
+	createTexture(&boxTexture, "texture\\container2.png", FALSE);
+	createTexture(&boxSpecular, "texture\\container2_specular.png", FALSE);
 	createTexture(&boxEmissive, "texture\\matrix.jpg", FALSE);
 
 	//because we use multiple texture set a number for each texture (cannot be the same)
@@ -196,7 +231,6 @@ int main()
 
 	vec3 redu = { 0.2f,0.2f,0.2f };
 	vec3 axeRota = { .5f, 1.f, 0.f };
-	vec3 lightPos = { 1.2f, 1.0f, 2.0f };
 
 	Camera camera = {
 		.view = GLM_MAT4_IDENTITY_INIT,
@@ -210,6 +244,20 @@ int main()
 		.lastY = (float)SCR_WIDTH / 2,
 		.lastFrame = 0.0f,
 		.deltaTime = 0.0f
+	};
+
+	Light light = {
+		.diffuse = { 0.5f, 0.5f, 0.5f },
+		.ambiant = { 0.2f, 0.2f, 0.2f },
+		.specular = { 1.f, 1.f, 1.f },
+		.direction = { -0.2f, -1.0f, -0.3f},
+		.position = { 0.0f, 1.0f, 5.0f },
+		.watcher = { camera.pos[0],camera.pos[2], camera.pos[2] },
+
+		.constant = 1.0f,
+		.linear = .09f,
+		.quadratic = 0.0032f
+
 	};
 
 	while (!glfwWindowShouldClose(window))
@@ -237,28 +285,8 @@ int main()
 		float time = glfwGetTime();
 		addFloat(shaderProgram, "time", time);
 
-		vec3 lightColor = { 1.0,1.0,1.0 };
-
-		//light struct
-		vec3 ldiffuse = { 0.5f, 0.5f, 0.5f };
-		//glm_vec3_mul(ldiffuse, lightColor, ldiffuse);
-		addVec3(shaderProgram, "light.diffuse", ldiffuse);
-		vec3 lambiant = { 0.2f, 0.2f, 0.2f };
-		//glm_vec3_mul(lambiant, ldiffuse, lambiant);
-		addVec3(shaderProgram, "light.ambient", lambiant);
-		vec3 lspecular = { 1.f, 1.f, 1.f };
-		addVec3(shaderProgram, "light.specular", lspecular);
-		vec3 ldirection = { -0.2f, -1.0f, -0.3f };
-		addVec3(shaderProgram, "light.direction", ldirection);
-		addVec3(shaderProgram, "light.position", lightPos);
-		addVec3(shaderProgram, "viewPos", camera.pos);
-		addFloat(shaderProgram, "light.constant", 1.0f);
-		addFloat(shaderProgram, "light.linear", .09f);
-		addFloat(shaderProgram, "light.quadratic", 0.0032f);
-		
-		//material struct
+		sendLightInfo(light, shaderProgram);
 		addFloat(shaderProgram, "material.shininess", 32.0f);
-
 
 		camera.deltaTime = time - camera.lastFrame;
 		camera.lastFrame = time;
@@ -268,12 +296,11 @@ int main()
 		glm_lookat(camera.pos, camera.target, camera.upAxe, camera.view);
 
 		mat4 projection = GLM_MAT4_IDENTITY_INIT;
-
+		//make the cam
 		glm_perspective(glm_rad(45.f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.f, projection);
-
 		//send the matrix to the shader
-		glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, camera.view[0]);
-		glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, projection[0]);
+		addMat4(shaderProgram, "view", camera.view);
+		addMat4(shaderProgram, "projection", projection);
 
 		//draw
 		glBindVertexArray(cubeVAO);
@@ -290,12 +317,10 @@ int main()
 		}
 
 		glUseProgram(lightProgram);
-		addVec3(shaderProgram, "Color", lightColor);
 		glBindVertexArray(lightVAO);
 		mat4 lightPosRedu = GLM_MAT4_IDENTITY_INIT;
-		lightPos[2] = sin(time) * 2;
-		lightPos[0] = cos(time) * 4;
-		glm_translate(lightPosRedu, lightPos);
+
+		glm_translate(lightPosRedu, light.position);
 		glm_scale(lightPosRedu, redu);
 
 		addMat4(lightProgram, "view", camera.view);
@@ -309,10 +334,13 @@ int main()
 		glfwPollEvents();
 	}
 
+	tinyobj_attrib_free(&attrib);
+	tinyobj_shapes_free(shapes, num_shapes);
+	tinyobj_materials_free(materials, num_materials);
+
 	glDeleteVertexArrays(1, &cubeVAO);
 	glDeleteVertexArrays(1, &lightProgram);
 	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
 
 	glfwTerminate();
 
